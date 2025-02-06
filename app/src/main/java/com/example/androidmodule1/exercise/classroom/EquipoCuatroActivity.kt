22deecb7432b3ce5b6a802ccd2ce2a4bc97b4676
talos <talos@unam.mx>
2025-02-05 18:18:45 -0600

    Intento de vista principal del Equipo 4 para referir a miembros

---
+++ app/src/main/java/com/example/androidmodule1/exercise/classroom/EquipoCuatroActivity.kt
@@ -0,0 +1,23 @@
+package com.example.androidmodule1.exercise.classroom
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidmodule1.R
+
+class EquipoCuatroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_equipo_cuatro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
